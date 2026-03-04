65d4b3b2a693097788173a997bdda7f9d94af745
Matheus Sapata <matheusultras@gmail.com>
2026-03-04 14:42:26 -0300

    add pixel geral

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly VITE_GLOBAL_META_PIXEL_ID?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
